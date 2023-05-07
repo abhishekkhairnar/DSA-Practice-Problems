b56e9ffb77237192ac8e718d9668ca26a63380fb
unknown <khairnarabhi25@gmail.com>
2023-05-07 23:03:40 +0530

    binary search

---
+++ Java/Literals.java
@@ -0,0 +1,10 @@
+public class Literals{
+    public static void main(String[] args) {
+        long i = 999_999_999_999_999_999L;
+        double d = 23.4f;
+        float f = 23f;
+        
+        System.out.println(f);
+
+    }
+}
--- searching/BinarySearch.java
+++ searching/BinarySearch.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class BinarySearch{
     public static void main(String[]args){
-        int[]arr={1,2,3,4,5};
-        System.out.println(binarysearch(arr,3));
+        int[]arr = new int[10];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 10 numbers : ");
+        for(int i=0;i<10;i++){
+            arr[i] = sc.nextInt();
+        }
+        int key=0;
+        System.out.println("Enter key you want to search : ");
+        key = sc.nextInt();
+        System.out.println(binarysearch(arr,key));
     }
     public static int binarysearch(int[]arr,int target){
         int start=0;
