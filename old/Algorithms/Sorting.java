eb82282ebc173762f9833219c27daf478e6ef728
abhishekkhairnar <abhishek.khairnar@noovosoft.com>
2025-06-28 12:30:26 +0530

    insertion sort

--- old/Algorithms/Sorting.java
+++ old/Algorithms/Sorting.java
@@ -5,8 +5,8 @@ public class Sorting {
         int [] arr = new int [] {9, 88, 0, 1, 2, 3, 4, 5, 6, 7}; // 23, 24, 26, 41, 47, 72, 73, 94
 
 //        selectionSort(arr);
-        bubbleSort(arr, 10);
-
+//        bubbleSort(arr, 10);
+        insertionSort(arr, 10);
         System.out.println(Arrays.toString(arr));
     }
     // time complexity = O(n^2)
@@ -49,5 +49,17 @@ public class Sorting {
         }
 
         System.out.println(count);
+    }
+
+    public static void insertionSort(int [] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                arr[j] += arr[j - 1];
+                arr[j - 1] = arr[j] - arr[j - 1];
+                arr[j] -= arr[j - 1];
+                j--;
+            }
+        }
     }
 }
